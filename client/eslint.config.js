53c6ae05050477ee148aa8f7698c1e2e121472e9
lesterfernandez <5958256+lesterfernandez@users.noreply.github.com>
2023-09-22 17:36:01 -0400

    update eslint to flat config

---
+++ client/eslint.config.js
@@ -0,0 +1,39 @@
+import js from "@eslint/js";
+import ts from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import { Linter } from "eslint";
+import hooks from "eslint-plugin-react-hooks";
+import globals from "globals";
+
+/**
+ * @type {Linter.FlatConfig[]}
+ */
+export default [
+  {
+    ...js.configs.recommended,
+    files: ["src/**/*.{js,jsx,ts,tsx}"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+    },
+  },
+  {
+    files: ["src/**/*.{ts,tsx}"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+    },
+    plugins: {
+      ts,
+      hooks,
+    },
+    rules: {
+      ...ts.rules.recommended,
+      "hooks/rules-of-hooks": "error",
+      "hooks/exhaustive-deps": "warn",
+    },
+  },
+];

baacbba46497248a4a1f876aa7194be2d2467b09
fnotorious <fnato001@fiu.edu>
2023-09-27 15:41:52 -0400

    routes done

---
+++ client/src/components/pages/Test.tsx
@@ -0,0 +1,7 @@
+const Test = () => {
+  return (
+    <div>You made it!</div>
+  )
+}
+
+export default Test
---
+++ client/src/components/pages/index.ts
@@ -0,0 +1,9 @@
+import Login from "./Login";
+import Register from "./Register";
+import Test from "./Test";
+
+export {
+    Login,
+    Register,
+    Test
+}

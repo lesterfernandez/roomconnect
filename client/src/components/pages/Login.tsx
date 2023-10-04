314bd1bc6e14157560071d5ebbe322ad2f54eeaf
fnotorious <fnato001@fiu.edu>
2023-10-04 16:02:59 -0400

    Removed 'signIn' use from Login.tsx

--- client/src/components/pages/Login.tsx
+++ client/src/components/pages/Login.tsx
@@ -1,20 +1,5 @@
-import { useNavigate } from "react-router-dom";
-import { useAuthStore } from "../../store";
-
 const Login = () => {
-  const login = useAuthStore(state => state.signIn);
-  const navigate = useNavigate();
-
-  const handleSubmit = () => {
-    login("user", "pass");
-    navigate("/");
-  };
-
-  return (
-    <div>
-      <button onClick={handleSubmit}>Login</button>
-    </div>
-  );
+  return <div>Login</div>;
 };
 
 export default Login;

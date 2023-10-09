4bac53aa816fef6bebdc83c519ebaed717ab6816
Jocelyn <28810573+Noodulz@users.noreply.github.com>
2023-10-09 14:54:14 -0400

    Update Login.tsx

--- client/src/components/pages/Login.tsx
+++ client/src/components/pages/Login.tsx
@@ -1,3 +1,23 @@
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  Radio,
+  RadioGroup,
+  Stack,
+  Select,
+  Button,
+  ButtonGroup,
+  Heading,
+  Box,
+  VStack,
+} 
+from "@chakra-ui/react";
+import { useState } from "react";
+import { UserCredientials } from "../../types";
+import { userCredentialsSchema } from "../../schemas";
+
+
 const Login = () => {
   return <div>Login</div>;
 };

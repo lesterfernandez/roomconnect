c2b0a858d69b0f67f97783372e76ea5795df5d18
Javier Daniel Garcia <89328228+javidangarcia@users.noreply.github.com>
2023-10-09 10:47:15 -0400

    added register fields and user interface (#16)

--- client/src/schemas.ts
+++ client/src/schemas.ts
@@ -7,9 +7,9 @@ export const apiErrorSchema = z.object({
 export const userProfileSchema = z
   .object({
     profilePic: z.string().optional(),
-    displayName: z.string(),
+    displayName: z.string().min(1),
     budget: z.literal(1).or(z.literal(2)).or(z.literal(3)).or(z.literal(4)),
-    gender: z.string(),
+    gender: z.string().min(1),
     cleanliness: z.literal(1).or(z.literal(2)).or(z.literal(3)),
     loudness: z.literal(1).or(z.literal(2)).or(z.literal(3)),
     coed: z.boolean(),
@@ -19,3 +19,11 @@ export const userProfileSchema = z
 export const tokenMessageSchema = z.object({
   token: z.string(),
 });
+
+export const userCredentialsSchema = z.object({
+  username: z.string().min(1),
+  password: z.string().min(1),
+});
+
+export const registerBodySchema = userProfileSchema.and(userCredentialsSchema);
+

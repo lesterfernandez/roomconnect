4bbfce33622935e833058db992be359f3f84f6d0
Jocelyn <28810573+Noodulz@users.noreply.github.com>
2023-10-09 15:21:50 -0400

    Update Login.tsx

--- client/src/components/pages/Login.tsx
+++ client/src/components/pages/Login.tsx
@@ -17,11 +17,34 @@ import { useState } from "react";
 import { UserCredientials } from "../../types";
 import { userCredentialsSchema } from "../../schemas";
 
+type Action = { type: 'loginSuccess', payload: string }
+| { type: 'loginFailed', payload: string };
 
 const Login = () => {
   const [loginBody, setLoginBody] = useState<UserCredientials>({
     username: "",
     password: ""
   });
+
+  const handleLogin = () => {
+    const parsedLoginBody = userCredentialsSchema.safeParse(UserCredientials);
+    if (!parsedLoginBody.success) {
+      alert("Login import failed!");
+      return;
+    }
+
+    if (loginBody.username !== '' && loginBody.password !== '') {
+      dispatch({
+        type: 'loginSuccess',
+        payload: 'Login Successfully'
+      }); 
+    }
+    else {
+      dispatch({
+        type: 'loginFailed',
+        payload: 'Incorrect username or password'
+      });
+    }
+  }
 };
 export default Login;

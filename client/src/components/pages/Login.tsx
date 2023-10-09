1b158f9d940ee712a77d79fc4c00c053d7437a60
Jocelyn <28810573+Noodulz@users.noreply.github.com>
2023-10-09 15:38:58 -0400

    Update Login.tsx

--- client/src/components/pages/Login.tsx
+++ client/src/components/pages/Login.tsx
@@ -32,15 +32,40 @@ const Login = () => {
       alert("Login import failed!");
       return;
     }
-    if (loginBody.username == '' || loginBody.password !== '') {
-      alert("Login failed!");
+    if (loginBody.username == '' || loginBody.password == '') {
+      alert("Please enter a username and password!");
       return;
     }
     alert(JSON.stringify(parsedLoginBody.data));
   }
 
   return (
-  
+    <Box bg="#156087">
+      <VStack maxW="2xl" textColor="white" padding="3rem" gap="4" marginInline="auto">
+        <Heading textAlign="center">Login</Heading>
+        <FormControl>
+          <FormLabel>Username</FormLabel>
+          <Input
+            type="text"
+            value={loginBody.username}
+            onChange={event => setLoginBody({ ...loginBody, username: event.target.value })}
+          />
+        </FormControl>
+        <FormControl>
+          <FormLabel>Password</FormLabel>
+          <Input
+            type="password"
+            value={loginBody.password}
+            onChange={event => setLoginBody({ ...loginBody, password: event.target.value })}
+          />
+        </FormControl>
+        <FormControl display="flex" justifyContent="center">
+          <Button colorScheme="orange" onClick={handleLogin}>
+            Login
+          </Button>
+        </FormControl>
+      </VStack>
+    </Box>
   );
 };
 export default Login;

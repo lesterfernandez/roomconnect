194dd17dd21559b6224dd5e8be5ffa383d290055
Lester <5958256+lesterfernandez@users.noreply.github.com>
2023-10-12 06:17:17 -0700

    move loading file (#29)
    * move loading file and remove unused files

--- client/src/components/pages/index.ts
+++ client/src/components/pages/index.ts
@@ -1,7 +1,6 @@
 import Login from "./Login";
 import Register from "./Register";
-import Test from "./Test";
-import Loading from "./Loading";
+import Loading from "../Loading";
 import NotFound from "./NotFound";
 
-export { Login, Register, Test, Loading, NotFound };
+export { Login, Register, Loading, NotFound };
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import { Login, Register, Test, Loading, NotFound } from "./components/pages";
+import { Login, Register, Loading, NotFound } from "./components/pages";
 
 import { createBrowserRouter, RouterProvider, redirect, Outlet } from "react-router-dom";
 
@@ -36,12 +36,6 @@ const router = createBrowserRouter([
       }
     },
     element: <Loading />,
-    children: [
-      {
-        index: true,
-        element: <Test />,
-      },
-    ],
     errorElement: <NotFound />,
   },
   {

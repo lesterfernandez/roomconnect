0103f5950c69035e759477a4bb9181329590030b
fnotorious <fnato001@fiu.edu>
2023-10-16 16:04:43 -0400

    Fixing implicit login.

--- client/src/components/Loading.tsx
+++ client/src/components/Loading.tsx
@@ -1,10 +1,23 @@
+import { Suspense } from "react";
+import { Await, useLoaderData } from "react-router-dom";
 import { Box, CircularProgress } from "@chakra-ui/react";
+import { Login } from "./pages";
 
 const Loading = () => {
+  const data = useLoaderData() as { response: null };
+
   return (
-    <Box bg="#156087" display="flex" minH="calc(100vh)">
-      <CircularProgress isIndeterminate color="white" m="auto" />
-    </Box>
+    <Suspense
+      fallback={
+        <Box bg="#156087" display="flex" minH="calc(100vh)">
+          <CircularProgress isIndeterminate color="white" m="auto" />
+        </Box>
+      }
+    >
+      <Await resolve={data.response} errorElement={<Login />}>
+        {() => <p>Hello</p>}
+      </Await>
+    </Suspense>
   );
 };
 

7d9917b888d2b3637970780cf08b9a81f798db22
Jocelyn <28810573+Noodulz@users.noreply.github.com>
2023-10-25 15:52:26 -0400

    Mapped out search results upon click (#55)
    * Create README.md
    
    * Wireframe of user profile page
    
    * Update Register.tsx
    
    * Update Login.tsx
    
    * Update Login.tsx
    
    * Update Login.tsx
    
    * Update Login.tsx
    
    * Update Login.tsx
    
    * Update Login.tsx
    
    * Update Login.tsx
    
    * Added select boxes and implemented search query to backend
    
    * Applied search schema and mapped out search results

--- client/src/schemas.ts
+++ client/src/schemas.ts
@@ -4,17 +4,21 @@ export const apiErrorSchema = z.object({
   errorMessage: z.string(),
 });
 
-export const userProfileSchema = z
-  .object({
-    profilePic: z.string().optional(),
-    displayName: z.string().min(1),
-    budget: z.literal(1).or(z.literal(2)).or(z.literal(3)).or(z.literal(4)),
-    gender: z.string().min(1),
-    cleanliness: z.literal(1).or(z.literal(2)).or(z.literal(3)),
-    loudness: z.literal(1).or(z.literal(2)).or(z.literal(3)),
-    coed: z.boolean(),
-  })
+const userProfileBodySchema = z
+.object({
+  profilePic: z.string().optional(),
+  budget: z.literal(1).or(z.literal(2)).or(z.literal(3)).or(z.literal(4)),
+  cleanliness: z.literal(1).or(z.literal(2)).or(z.literal(3)),
+  coed: z.boolean(),
+  displayName: z.string().min(1),
+  gender: z.string().min(1),
+  loudness: z.literal(1).or(z.literal(2)).or(z.literal(3)),
+});
+
+export const userProfileSchema = userProfileBodySchema
   .or(apiErrorSchema);
+
+export const searchResultSchema = z.array(userProfileBodySchema);
 
 export const tokenMessageSchema = z.object({
   token: z.string(),

c57b34649dbcd6d02427eeb1a19dc61a98d14164
fnotorious <fnato001@fiu.edu>
2023-10-30 16:05:29 -0400

    fix navbar

--- client/src/components/ui/Navbar.tsx
+++ client/src/components/ui/Navbar.tsx
@@ -1,28 +1,31 @@
 import { Box, Text, Flex } from "@chakra-ui/react";
-import { Link } from "react-router-dom";
-import { useLocation } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
-  const location = useLocation();
-
   return (
     <Box bg="#01293f">
       <Flex p="5" gap="40px" m="0px 40px">
-        <Link to="/">
-          <Text color={location.pathname == "/" ? "#fca95a" : "white"} fontSize="xl">
-            Search
-          </Text>
-        </Link>
-        <Link to="/profile">
-          <Text color={location.pathname == "/profile" ? "#fca95a" : "white"} fontSize="xl">
-            Profile
-          </Text>
-        </Link>
-        <Link to="/chat">
-          <Text color={location.pathname == "/chat" ? "#fca95a" : "white"} fontSize="xl">
-            Chat
-          </Text>
-        </Link>
+        <NavLink to="/">
+          {({ isActive }) => (
+            <Text color={isActive ? "#fca95a" : "white"} fontSize="xl">
+              Search
+            </Text>
+          )}
+        </NavLink>
+        <NavLink to="/profile">
+          {({ isActive }) => (
+            <Text color={isActive ? "#fca95a" : "white"} fontSize="xl">
+              Profile
+            </Text>
+          )}
+        </NavLink>
+        <NavLink to="/chat">
+          {({ isActive }) => (
+            <Text color={isActive ? "#fca95a" : "white"} fontSize="xl">
+              Chat
+            </Text>
+          )}
+        </NavLink>
       </Flex>
     </Box>
   );
--- client/src/store.ts
+++ client/src/store.ts
@@ -20,5 +20,5 @@ export const useSearchStore = create<SearchStore>(() => ({
     loudness: "",
     coed: "",
   },
-  results: []
-}))
+  results: [],
+}));

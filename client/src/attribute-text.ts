482faa84b154a239dd72654a6bec87a0388c7287
Firas Natour <42959543+fnotorious@users.noreply.github.com>
2023-11-02 19:07:08 -0400

    Redesign search card (#69)
    * Redesign user card
    
    * Removed position relative, fixed a minor bug.
    
    ---------
    
    Co-authored-by: Lester <5958256+lesterfernandez@users.noreply.github.com>

---
+++ client/src/attribute-text.ts
@@ -0,0 +1,11 @@
+const cleanliness = ["Messy", "Average", "Cleanfreak"];
+const budget = ["1000", "1000-2000", "2000+"];
+const loudness = ["Quiet", "Average", "Party Animal"];
+const coed = ["No", "Yes"];
+
+export default {
+  cleanliness,
+  budget,
+  loudness,
+  coed,
+};
--- client/src/components/search/UserCard.tsx
+++ client/src/components/search/UserCard.tsx
@@ -1,25 +1,51 @@
-import { Card, Stack, CardBody, Heading, Text, Box, Button, Avatar } from "@chakra-ui/react";
+import {
+  Card,
+  Stack,
+  CardBody,
+  Heading,
+  Tag,
+  Box,
+  Button,
+  Avatar,
+  Wrap,
+  WrapItem,
+} from "@chakra-ui/react";
 import type { UserAttributes } from "../../types";
+import attributes from "../../attribute-text";
 
 export default function UserCard(props: { profile: UserAttributes }) {
   return (
-    <Card direction={{ base: "column", sm: "row" }} variant="outline">
+    <Card direction={{ base: "column", sm: "row" }} p="2" gap={2} variant="outline">
       <Box margin="20px" display="flex" justifyContent="center" alignItems="center">
         <Avatar mx="auto" size="xl" src={props.profile.profilePic} />
       </Box>
 
       <Stack>
         <CardBody px="0">
-          <Heading size="md">{props.profile.displayName}</Heading>
-          <Text>Gender: {props.profile.gender}</Text>
-          <Text>Budget: {props.profile.budget}</Text>
-          <Text>Loudness: {props.profile.loudness}</Text>
-          <Text>Cleanliness: {props.profile.cleanliness}</Text>
-          <Text>Co-Ed: {props.profile.cleanliness}</Text>
+          <Heading size="md" mb={2}>
+            {props.profile.displayName}
+          </Heading>
+          <Wrap gap={2}>
+            <WrapItem>
+              <Tag>Gender: {props.profile.gender}</Tag>
+            </WrapItem>
+            <WrapItem>
+              <Tag>Budget: {attributes.budget[props.profile.budget]}</Tag>
+            </WrapItem>
+            <WrapItem>
+              <Tag>Loudness: {attributes.loudness[props.profile.loudness]}</Tag>
+            </WrapItem>
+            <WrapItem>
+              <Tag>Cleanliness: {attributes.cleanliness[props.profile.cleanliness]}</Tag>
+            </WrapItem>
+            <WrapItem>
+              <Tag>Co-Ed: {attributes.coed[+props.profile.coed]}</Tag>
+            </WrapItem>
+          </Wrap>
         </CardBody>
       </Stack>
 
-      <Button position="absolute" bottom="10px" right="10px" colorScheme="orange">
+      <Button colorScheme="orange" ml="auto" mt="auto" minW={{ base: "100%", sm: "5rem" }}>
         Chat
       </Button>
     </Card>

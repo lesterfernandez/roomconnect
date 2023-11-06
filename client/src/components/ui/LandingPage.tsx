bdcdf1c3626d9c55dd0bdc2ee0338fe6690f96a0
Jocelyn <28810573+Noodulz@users.noreply.github.com>
2023-11-06 15:30:04 -0500

    Converted landing page to Chakra UI

--- client/src/components/ui/LandingPage.tsx
+++ client/src/components/ui/LandingPage.tsx
@@ -1,13 +1,52 @@
-import "./css/LandingPage.css";
+import { ChakraProvider, extendTheme, VStack, Text, CSSReset } from "@chakra-ui/react";
+import "css/LandingPage.css";
 
-export default function LandingPage() {
+const theme = extendTheme({
+  fonts: {
+    heading: `'Michroma'`,
+    body: `'Michroma'`,
+  },
+  colors: {
+    primary: {
+      100: "#D3DEE1",
+      900: "#00293F",
+    },
+  },
+  styles: {
+    global: {
+      "html, body": {
+        height: "100%",
+        margin: 0,
+        padding: 3,
+      },
+      body: {
+        bgGradient: "linear(to-b, primary.900, 55%, primary.100)",
+      },
+    },
+  },
+});
+
+export default function App() {
   return (
-    <main>
-      <h1 className="greeting">
-        Find your <br />
-        <span>roommates in</span> <br />
-        Miami
-      </h1>
-    </main>
+    <ChakraProvider theme={theme}>
+      <CSSReset />
+      <VStack
+        height="100%"
+        align="start"
+        justify="left"
+        paddingLeft="2em"
+        position="relative"
+        top="60px"
+      >
+        <Text fontFamily="heading" fontSize="2.5em" textAlign="left" lineHeight="1.2" color="white">
+          Find your <br />
+          <Text as="span" color="white">
+            roommates in
+          </Text>
+          <br />
+          Miami
+        </Text>
+      </VStack>
+    </ChakraProvider>
   );
 }

1b4d45f6895b693c399c21ea51be040799d5f8c2
lesterfernandez <5958256+lesterfernandez@users.noreply.github.com>
2023-11-09 13:14:30 -0500

    load page with initial results

--- client/src/components/search/Search.tsx
+++ client/src/components/search/Search.tsx
@@ -1,32 +1,45 @@
-import { FormControl, FormLabel, Button, Select, Container, Stack, Flex } from "@chakra-ui/react";
-import React, { useState } from "react";
+import {
+  FormControl,
+  FormLabel,
+  Button,
+  Select,
+  Container,
+  Stack,
+  Flex,
+  Spinner,
+} from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
 import UserCard from "./UserCard";
 import { searchUsers } from "../../api/search";
 import type { SearchBody } from "../../types";
 import { useSearchStore } from "../../store/search";
 import attributes from "../../attribute-text";
 
+const handleSearch = async (settings: Partial<SearchBody>) => {
+  try {
+    const results = await searchUsers(settings);
+    useSearchStore.setState({ results });
+  } catch (error) {
+    console.error("Error fetching search results:", error);
+  }
+};
+
+const handleChange = (name: keyof SearchBody) => (e: React.ChangeEvent<HTMLSelectElement>) => {
+  useSearchStore.setState({
+    settings: { [name]: e.target.value },
+  });
+};
+
 export default function Search() {
   const { settings, results } = useSearchStore();
   const [searchLoading, setSearchLoading] = useState(false);
+  const [initialLoading, setInitialLoading] = useState(results.length === 0);
 
-  const handleSearch = async () => {
-    setSearchLoading(true);
-    try {
-      const results = await searchUsers(settings);
-      useSearchStore.setState({ results });
-    } catch (error) {
-      console.error("Error fetching search results:", error);
-    } finally {
-      setSearchLoading(false);
+  useEffect(() => {
+    if (useSearchStore.getState().results.length === 0) {
+      handleSearch({}).finally(() => setInitialLoading(false));
     }
-  };
-
-  const handleChange = (name: keyof SearchBody) => (e: React.ChangeEvent<HTMLSelectElement>) => {
-    useSearchStore.setState({
-      settings: { [name]: e.target.value },
-    });
-  };
+  }, []);
 
   return (
     <Container maxW="container.lg" px="4">
@@ -43,8 +56,8 @@ export default function Search() {
             value={settings.budget}
             onChange={handleChange("budget")}
           >
-            {attributes.budget.map((option, index) => {
-              return <option label={option} value={index + 1}></option>;
+            {attributes.budget.map((option, i) => {
+              return <option label={option} value={i + 1} key={`select-budget-${i}`} />;
             })}
           </Select>
         </FormControl>
@@ -56,8 +69,8 @@ export default function Search() {
             value={settings.cleanliness}
             onChange={handleChange("cleanliness")}
           >
-            {attributes.cleanliness.map((option, index) => {
-              return <option label={option} value={index + 1}></option>;
+            {attributes.cleanliness.map((option, i) => {
+              return <option label={option} value={i + 1} key={`select-cleanliness-${i}`} />;
             })}
           </Select>
         </FormControl>
@@ -69,8 +82,8 @@ export default function Search() {
             value={settings.loudness}
             onChange={handleChange("loudness")}
           >
-            {attributes.loudness.map((option, index) => {
-              return <option label={option} value={index + 1}></option>;
+            {attributes.loudness.map((option, i) => {
+              return <option label={option} value={i + 1} key={`select-loudness-${i}`} />;
             })}
           </Select>
         </FormControl>
@@ -88,18 +101,30 @@ export default function Search() {
             colorScheme="orange"
             width="100%"
             borderRadius="6px"
-            onClick={handleSearch}
+            onClick={async () => {
+              setSearchLoading(true);
+              await handleSearch(settings);
+              setSearchLoading(false);
+            }}
+            isDisabled={searchLoading}
             isLoading={searchLoading}
           >
             Search
           </Button>
         </FormControl>
       </Flex>
-      <Stack gap="6" pb="6">
-        {results.map((result, i) => (
-          <UserCard profile={result} key={`card-${i}`} />
-        ))}
-      </Stack>
+
+      {initialLoading ? (
+        <Flex justify="center">
+          <Spinner size="lg" />
+        </Flex>
+      ) : (
+        <Stack gap="6" pb="6">
+          {results.map((result, i) => (
+            <UserCard profile={result} key={`card-${i}`} />
+          ))}
+        </Stack>
+      )}
     </Container>
   );
 }

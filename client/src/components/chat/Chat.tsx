6c672f18aa9edc7bae4e04960d55bb7c96c673c3
lesterfernandez <5958256+lesterfernandez@users.noreply.github.com>
2023-11-09 13:14:13 -0500

    add text for empty chat

--- client/src/components/chat/Chat.tsx
+++ client/src/components/chat/Chat.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import { Outlet } from "react-router-dom";
-import { HStack, VStack, StackDivider, Text, Button } from "@chakra-ui/react";
+import { HStack, VStack, StackDivider, Text, Button, Center } from "@chakra-ui/react";
 import { useMessageStore } from "../../store/message";
 import useChatSetup from "./useChat";
 import { createContext } from "react";
@@ -13,44 +13,50 @@ export const ChatContext = createContext<ChatContextType>({} as ChatContextType)
 export default function Chat() {
   const messages = useMessageStore();
   const { handleSendMessage } = useChatSetup();
-
   return (
     <ChatContext.Provider value={{ handleSendMessage }}>
-      <HStack h="full" gap="0">
-        <VStack
-          divider={<StackDivider borderColor="gray.200" my="0 !important" />}
-          w="18rem"
-          h="100%"
-          align="stretch"
-          borderWidth="thin"
-          borderColor="gray.200"
-          overflow="scroll"
-        >
-          {Object.keys(messages).map(username => (
-            <Link to={`/chat/${username}`} key={username}>
-              <Button
-                w="100%"
-                h="12"
-                variant="unstyled"
-                textAlign="start"
-                px="4"
-                py="1"
-                rounded="none"
-                _hover={{
-                  background: "gray.100",
-                  transitionDuration: "0.2s",
-                  transitionTimingFunction: "ease-in-out",
-                }}
-              >
-                <Text fontSize="sm" as="b">
-                  {username}
-                </Text>
-              </Button>
-            </Link>
-          ))}
-        </VStack>
-        <Outlet />
-      </HStack>
+      {Object.keys(messages).length ? (
+        <HStack h="full" gap="0">
+          <VStack
+            divider={<StackDivider borderColor="gray.200" my="0 !important" />}
+            w="18rem"
+            h="100%"
+            align="stretch"
+            borderWidth="thin"
+            borderColor="gray.200"
+            overflow="scroll"
+          >
+            {Object.keys(messages).map(username => (
+              <Link to={`/chat/${username}`} key={username}>
+                <Button
+                  w="100%"
+                  h="12"
+                  variant="unstyled"
+                  textAlign="start"
+                  px="4"
+                  py="1"
+                  rounded="none"
+                  _hover={{
+                    background: "gray.100",
+                    transitionDuration: "0.2s",
+                    transitionTimingFunction: "ease-in-out",
+                  }}
+                >
+                  <Text fontSize="sm" as="b">
+                    {username}
+                  </Text>
+                </Button>
+              </Link>
+            ))}
+          </VStack>
+          <Outlet />
+        </HStack>
+      ) : (
+        <Center flexDir="column" h="100%">
+          <Text fontSize="lg">No conversations here...</Text>
+          <Text fontSize="lg">Search for roommates and start chatting!</Text>
+        </Center>
+      )}
     </ChatContext.Provider>
   );
 }

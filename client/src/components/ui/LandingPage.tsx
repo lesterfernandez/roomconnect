5f850ffbcc7e41b84ecc87ec329b006d6de48a64
Jocelyn <28810573+Noodulz@users.noreply.github.com>
2023-11-13 16:13:00 -0500

    Fixed styling to only apply to LandingPage.tsx

--- client/src/components/ui/LandingPage.tsx
+++ client/src/components/ui/LandingPage.tsx
@@ -1,64 +1,33 @@
-import "assets/palm-tree-3d.png";
+import { ChakraProvider, VStack, Text, Box, Image, Flex, Button, Link } from "@chakra-ui/react";
 import "css/LandingPage.css";
-import {
-  ChakraProvider,
-  extendTheme,
-  VStack,
-  Text,
-  CSSReset,
-  Box,
-  Image,
-  Flex,
-  Button,
-} from "@chakra-ui/react";
+import "assets/palm-tree-3d.png";
 
-const theme = extendTheme({
-  fonts: {
-    heading: `'Michroma'`,
-    body: `'Michroma'`,
-  },
-  colors: {
-    primary: {
-      100: "#D3DEE1",
-      900: "#00293F",
-    },
-  },
-  styles: {
-    global: {
-      "html, body": {
-        height: "100%",
-        margin: 0,
-        padding: 3,
-        overflow: "hidden",
-      },
-      body: {
-        bgGradient: "linear(to-b, primary.900, 55%, primary.100)",
-      },
-    },
-  },
-});
-
-export default function App() {
+export default function LandingPage() {
   return (
-    <ChakraProvider theme={theme}>
-      <CSSReset />
-      <Flex justifyContent="flex-end">
+    <ChakraProvider>
+      <Flex justifyContent="flex-end" p={4}>
         <Button borderRadius="20px">Login</Button>
       </Flex>
       <Flex
         direction={{ base: "column", md: "row" }}
         justify="space-between"
         align={{ base: "center", md: "flex-start" }}
-        p="4"
+        p={4}
+        bgGradient="linear(to-b, #00293F, 55%, #D3DEE1)"
+        height="100vh"
       >
-        <VStack align="start" spacing="4" paddingLeft="2em">
+        <VStack
+          align="start"
+          spacing={4}
+          pl={{ base: "1em", md: "2em" }}
+          pt={{ base: "1em", md: "2.5em" }}
+        >
           <Text
-            fontFamily="heading"
             fontSize="2.5em"
             textAlign="left"
             lineHeight="1.2"
-            paddingTop="2.5em"
             color="white"
+            fontFamily="'Michroma', sans-serif"
           >
             Find your <br />
             <Text as="span" color="white">
@@ -67,12 +36,14 @@ export default function App() {
             <br />
             Miami
           </Text>
-          <Button colorScheme="orange" borderRadius="20px" mt="5">
-            Get Started
-          </Button>
+          <Link href="/login" style={{ textDecoration: "none" }}>
+            <Button colorScheme="orange" borderRadius="20px" size="lg">
+              Get Started
+            </Button>
+          </Link>
         </VStack>
         <Box display={{ base: "none", md: "block" }} alignSelf="flex-end" pr="20">
-          <Image src="src/assets/palm-tree-3d.png" boxSize="sm" />
+          <Image src="assets/palm-tree-3d.png" boxSize="sm" />
         </Box>
       </Flex>
     </ChakraProvider>

cc4af7ef569b3151561d20beccb8c4d7a28872bf
Jocelyn <28810573+Noodulz@users.noreply.github.com>
2023-11-13 15:24:04 -0500

    Set up landing page

--- client/src/components/ui/LandingPage.tsx
+++ client/src/components/ui/LandingPage.tsx
@@ -1,5 +1,16 @@
-import { ChakraProvider, extendTheme, VStack, Text, CSSReset } from "@chakra-ui/react";
+import "assets/palm-tree-3d.png";
 import "css/LandingPage.css";
+import {
+  ChakraProvider,
+  extendTheme,
+  VStack,
+  Text,
+  CSSReset,
+  Box,
+  Image,
+  Flex,
+  Button,
+} from "@chakra-ui/react";
 
 const theme = extendTheme({
   fonts: {
@@ -18,6 +29,7 @@ const theme = extendTheme({
         height: "100%",
         margin: 0,
         padding: 3,
+        overflow: "hidden",
       },
       body: {
         bgGradient: "linear(to-b, primary.900, 55%, primary.100)",
@@ -30,23 +42,39 @@ export default function App() {
   return (
     <ChakraProvider theme={theme}>
       <CSSReset />
-      <VStack
-        height="100%"
-        align="start"
-        justify="left"
-        paddingLeft="2em"
-        position="relative"
-        top="60px"
+      <Flex justifyContent="flex-end">
+        <Button borderRadius="20px">Login</Button>
+      </Flex>
+      <Flex
+        direction={{ base: "column", md: "row" }}
+        justify="space-between"
+        align={{ base: "center", md: "flex-start" }}
+        p="4"
       >
-        <Text fontFamily="heading" fontSize="2.5em" textAlign="left" lineHeight="1.2" color="white">
-          Find your <br />
-          <Text as="span" color="white">
-            roommates in
+        <VStack align="start" spacing="4" paddingLeft="2em">
+          <Text
+            fontFamily="heading"
+            fontSize="2.5em"
+            textAlign="left"
+            lineHeight="1.2"
+            paddingTop="2.5em"
+            color="white"
+          >
+            Find your <br />
+            <Text as="span" color="white">
+              roommates in
+            </Text>
+            <br />
+            Miami
           </Text>
-          <br />
-          Miami
-        </Text>
-      </VStack>
+          <Button colorScheme="orange" borderRadius="20px" mt="5">
+            Get Started
+          </Button>
+        </VStack>
+        <Box display={{ base: "none", md: "block" }} alignSelf="flex-end" pr="20">
+          <Image src="src/assets/palm-tree-3d.png" boxSize="sm" />
+        </Box>
+      </Flex>
     </ChakraProvider>
   );
 }

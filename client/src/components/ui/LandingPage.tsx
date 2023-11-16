bc047ce4baa396f88efb4ebf3604c0dd171b52a0
lesterfernandez <5958256+lesterfernandez@users.noreply.github.com>
2023-11-16 09:04:27 -0500

    edit landing page

--- client/src/components/ui/LandingPage.tsx
+++ client/src/components/ui/LandingPage.tsx
@@ -5,49 +5,47 @@ import img from "./assets/palm-tree-3d.png";
 export default function LandingPage() {
   return (
     <ChakraProvider>
-      <Flex
-        direction={{ base: "column", md: "row" }}
-        justify={{ base: "center", md: "center" }} 
-        align="center" 
-        bgGradient="linear(to-b, #00293F, 55%, #D3DEE1)"
-        height="100vh"
-        position="relative" 
-        p={4}
-      >
-        <Box position="absolute" top={4} right={4}>
-          <Link href="/login" style={{ textDecoration: "none" }}>
-            <Button borderRadius="20px">Login</Button>
-          </Link>
-        </Box>
-        <VStack
-          align="left"
-          spacing={4}
-          pt={{ base: "1em", md: "2.5em" }}
+      <Box bgGradient="linear(to-b, #00293F, 35%, #D3DEE1)" height="100vh">
+        <Flex
+          direction={{ base: "column", md: "row" }}
+          h="100%"
+          justify="space-around"
+          maxW="container.xl"
+          align="center"
+          mx="auto"
+          p={4}
         >
-          <Text
-            fontSize="3.0em"
-            textAlign="left"
-            lineHeight="1.2"
-            color="white"
-            fontFamily="'Michroma', sans-serif"
-          >
-            Find your <br />
-            <Text as="span" color="white">
-              roommates in
+          <Box position="absolute" top={4} right={4}>
+            <Link href="/login" style={{ textDecoration: "none" }}>
+              <Button>Login</Button>
+            </Link>
+          </Box>
+          <VStack align="left" spacing={4} pt={{ base: "1em", md: "2.5em" }}>
+            <Text
+              fontSize="3.0em"
+              textAlign="left"
+              lineHeight="1.2"
+              color="white"
+              fontFamily="'Michroma', sans-serif"
+            >
+              Find your <br />
+              <Text as="span" color="white">
+                roommates in
+              </Text>
+              <br />
+              Miami
             </Text>
-            <br />
-            Miami
-          </Text>
-          <Link href="/register" style={{ textDecoration: "none" }}>
-            <Button colorScheme="orange" borderRadius="20px" size="lg">
-              Get Started
-            </Button>
-          </Link>
-        </VStack>
-        <Box top={30} display={{ base: "none", md: "block" }} alignSelf="flex-end" pr="15">
-          <Image src={img} boxSize="sm" />
-        </Box>
-      </Flex>
+            <Link href="/register" style={{ textDecoration: "none" }}>
+              <Button colorScheme="orange" size="lg">
+                Get Started
+              </Button>
+            </Link>
+          </VStack>
+          <Box display={{ base: "none", md: "block" }} pr="15">
+            <Image src={img} boxSize="sm" />
+          </Box>
+        </Flex>
+      </Box>
     </ChakraProvider>
   );
 }
